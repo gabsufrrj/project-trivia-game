6ec460186cdd65fcd84b4612df767c05509a1cd8
Caique Moreira <caique_moreira_sz@hotmail.com>
2022-04-06 14:56:32 -0300

    Requisito 3 concluido

---
+++ src/pages/Config.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Config extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 data-testid="settings-title">Configurações</h1>
+      </div>
+    );
+  }
+}
+
+export default Config;
