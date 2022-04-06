f5428e7ff1f50099168ea5f2b1ebbb890cb78a20
Caique Moreira <caique_moreira_sz@hotmail.com>
2022-04-06 16:00:56 -0300

    Requisito 4 Concluido

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Header from '../components/Header';
 
 class Game extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         GAME
       </div>
     );
