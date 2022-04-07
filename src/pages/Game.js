16b94ad998b371371678fd182399fa6d290ff93c
Caique Moreira <caique_moreira_sz@hotmail.com>
2022-04-07 18:26:18 -0300

    Quinto e Sexto requisito concluídos

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -10,9 +10,18 @@ class Game extends React.Component {
     super();
     this.state = {
       questionIndex: 0,
-      answers: this.shuffleAnswers(),
+      answers: [],
     };
   }
+
+  componentDidMount() {
+    this.shuffleAnswers();
+    // this.handleNextQuestion();
+  }
+
+  // componentDidUpdate() {
+  //   this.shuffleAnswers();
+  // }
 
   handleNextQuestion = () => {
     const { history } = this.props;
@@ -24,22 +33,29 @@ class Game extends React.Component {
       if (questionIndex >= indexNumber) {
         history.push('/feedback');
       }
+      this.shuffleAnswers();
     });
   }
 
   shuffleAnswers = () => {
     const { questions } = this.props;
     const { questionIndex } = this.state;
+    console.log(questionIndex);
     const incorrectAnswers = questions[questionIndex].incorrect_answers;
-    const correctAnswer = questions[questionIndex].correct_answers;
+    const correctAnswer = questions[questionIndex].correct_answer;
     const POINT_FIVE = 0.5;
     const allAnswers = [...incorrectAnswers, correctAnswer];
-    return allAnswers.sort(() => Math.random() - POINT_FIVE);
+    const allAnswersSorted = allAnswers.sort(() => Math.random() - POINT_FIVE);
+    this.setState(() => ({
+      answers: allAnswersSorted,
+    }));
   }
 
   render() {
-    const { questionIndex } = this.state;
+    const { questionIndex, answers } = this.state;
     const { questions } = this.props;
+    console.log(questionIndex);
+    // console.log(answers);
     return (
       <div>
         <Header />
@@ -47,11 +63,27 @@ class Game extends React.Component {
           {questions[questionIndex] && questions[questionIndex].category}
         </h1>
         <p
-          data-testid="question-test"
+          data-testid="question-text"
         >
-          {questions[questionIndex].question}
+          {questions[questionIndex] && questions[questionIndex].question}
 
         </p>
+        <div data-testid="answer-options">
+          {answers.map((ans, index) => (
+            <>
+              <button
+                key={ index }
+                type="button"
+                data-testid={ ans === questions[questionIndex].correct_answer
+                  ? 'correct-answer'
+                  : `wrong-answer-${index}` }
+              >
+                { ans }
+              </button>
+              <br />
+            </>
+          ))}
+        </div>
         <button
           data-testid="btn-next"
           type="button"
@@ -75,8 +107,8 @@ const mapStateToProps = (state) => ({
 Game.propTypes = {
   questions: PropTypes.string.isRequired,
   history: PropTypes.objectOf(PropTypes.any).isRequired,
-  correctAnswer: PropTypes.string.isRequired,
-  incorrectAnswers: PropTypes.arrayOf(PropTypes.string).isRequired,
+  // correctAnswer: PropTypes.string.isRequired,
+  // incorrectAnswers: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default connect(mapStateToProps)(Game);
