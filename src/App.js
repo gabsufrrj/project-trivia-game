d8fcc89d4fe99021b23caa25e1ad9bc157759e45
gabrieldezena10 <gabriel.deztor@gmail.com>
2022-04-10 16:45:25 -0300

    Requisito 12 e ajustes necessários

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Game from './pages/Game';
 import Config from './pages/Config';
+import Feedback from './pages/Feedback';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route exact path="/" component={ Home } />
         <Route exact path="/game" component={ Game } />
         <Route exact path="/config" component={ Config } />
+        <Route exact path="/feedback" component={ Feedback } />
       </Switch>
     </div>
   );
---
+++ src/pages/Feedback.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+
+class Feedback extends Component {
+  render() {
+    return (
+      <div data-testid="feedback-text">
+        <Header />
+
+      </div>
+
+    );
+  }
+}
+
+export default Feedback;
